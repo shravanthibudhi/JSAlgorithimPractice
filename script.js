481ff9dd38c0d1632a01cdbb14c139f7cff7f5c7
Shravanthi Budhi <66373469+shravanthibudhi@users.noreply.github.com>
2021-06-02 11:18:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ console.log(convertMinutesToSeconds(3));
 function yourAgeInSeconds (age) {
     return age * 12 * 30 * 24 * 60 * 60;
 }
-// console.log(yourAgeInSeconds(22));
+/*
+console.log(yourAgeInSeconds(22));
+*/
 
 // Get first item in array
 
@@ -37,4 +39,69 @@ let movies = [
     'Inception', 'Godfather', 
     'The Matrix', 'The Girl on the Train'
 ]
+/*
 console.log(getFirstItem(movies))
+*/
+
+// Label Good Movies (7-10) and Bad Movies (<6)
+
+function badOrGoodMovie(movie_rating) {
+    let movie_result;
+    if (movie_rating >= 7) {
+        movie_result = 'Good movie'
+    } else {
+        movie_result = 'Bad movie'
+    }
+    return movie_result;
+}
+/*
+console.log(badOrGoodMovie(6)); // bad
+console.log(badOrGoodMovie(7)); // good
+console.log(badOrGoodMovie(9.2)); // good
+console.log(badOrGoodMovie(2)); // bad
+*/
+
+// Check if a string is empty
+
+function isEmptyString(some_string) {
+    if (some_string == "") {
+        return true
+    } else {
+        return false
+    }
+}
+/*
+console.log(isEmptyString("")); // Empty
+console.log(isEmptyString("The Godfather")); // Not True
+*/
+
+// Finding the minimum
+
+function findMin(numbers) {
+    let minimum = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < minimum) {
+            minimum = numbers[i]
+        }
+    }
+    return minimum
+}
+/*
+console.log(findMin([5,2,9,8,7,3]))
+console.log(findMin([5,8,9,8,7,3]))
+*/
+
+// Finding the maximum
+function findMax(numbers) {
+    let maximum = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > maximum) {
+            maximum = numbers[i]
+        }
+    }
+    return maximum
+}
+/*
+console.log(findMax([5,2,9,8,7,3,13]))
+console.log(findMax([5,8,9,8,7,3]))
+*/
